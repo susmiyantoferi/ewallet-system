0268b83c578fd5ed38b24dbd891ca24ba539c8c8
Feri Susmiyanto <irefmcr@gmail.com>
2026-03-15 13:31:47 +0700

    fix : add constraint check and restrict delete

--- internal/entity/wallet.go
+++ internal/entity/wallet.go
@@ -11,8 +11,8 @@ import (
 type Wallet struct {
 	ID        uuid.UUID       `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
 	UserID    uuid.UUID       `gorm:"type:uuid;notnull;uniqueIndex:user_currency" json:"user_id"`
-	User      User            `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"user"`
-	Balance   decimal.Decimal `gorm:"type:decimal(20,2);notnull" json:"balance"`
+	User      User            `gorm:"foreignKey:UserID;constraint:OnDelete:RESTRICT" json:"user"`
+	Balance   decimal.Decimal `gorm:"type:decimal(20,2);notnull;check:balance>=0" json:"balance"`
 	Currency  string          `gorm:"type:varchar(3);notnull;uniqueIndex:user_currency" json:"currency"`
 	Status    WalletStatus    `gorm:"type:varchar(20);notnull;default:ACTIVE" json:"status"`
 	CreatedAt time.Time       `gorm:"type:timestamptz;default:CURRENT_TIMESTAMP" json:"created_at"`
